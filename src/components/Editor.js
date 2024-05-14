53631c8b3d62367a7b410be9ba0dd1c079533885
mbhasin1 <mbhasin@purdue.edu>
2024-05-14 15:37:44 -0700

    high score

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AceEditor from 'react-ace';
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-monokai";
@@ -7,10 +7,34 @@ import "ace-builds/src-noconflict/theme-monokai";
 function Editor(user) {
     const [code, setCode] = useState("#Instructions: A knight and a pawn are on a chess board. \n#Can you figure out the minimum number of moves required for the knight to travel to the same position of the pawn? \n#On a single move, the knight can move in an 'L' shape; two spaces in any direction, then one space in a perpendicular direction.\n#This means that on a single move, a knight has eight possible positions it can move to. (see end of document for a picture) \n#Write a function, knight_attack, that takes in 5 arguments: n, kr, kc, pr, pc\n#n = the length of the chess board kr = the starting row of the knight kc = the starting column of the knight pr = the row of the pawn\n#pc = the column of the pawn The function should return a number representing the minimum number of moves required for the knight to land on top of the pawn. \n#The knight cannot move out of bounds of the board.\n#You can assume that rows and columns are 0-indexed. This means that if n = 8, there are 8 rows and 8 columns numbered 0 to 7. \n#If it is not possible for the knight to attack the pawn, then return None.\ndef knight_attack(n, kr, kc, pr, pc):\n    # Your code here");
 const [results, setResults] = useState(null);
+const [highScore, setHighScore] = useState(null);
+
+
+    const fetchHighScore = async () => {
+        try {
+            //const response = await fetch('http://127.0.0.1/getHighScore', {
+            const response = await fetch('https://cs218-finalbackend.vercel.app/getHighScore', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ user: user.user.username, questionName: "KnightAttack" })
+            });
+            const result = await response.json();
+            console.log(result)
+            setHighScore(result);
+        } catch (error) {
+            console.error('Error fetching high score:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchHighScore();
+    }, []);
+
 
     const handleSubmit = async (code, user) => {
 
         console.log(code, user.username)
+        //const response = await fetch('http://127.0.0.1/runCode', {
         const response = await fetch('https://cs218-finalbackend.vercel.app/runCode', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -43,7 +67,12 @@ const [results, setResults] = useState(null);
                 <button onClick={() => handleSubmit(code, user.user)} style={{ marginTop: '20px' }}>Submit Code</button>
             
 
-
+                {highScore !== null && (
+                <div>
+                    <h3>High Score: {highScore}</h3>
+                   
+                </div>
+            )}
                 {results && (
                 <div>
                     <h3>Test Results:</h3>
